728ec6510c65da413a09836220b647f4661ef8eb
Vance-M <myveryvills@yahoo.com>
2021-11-28 14:29:16 -0800

    testing fix on export

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Alexa = require('ask-sdk-core');
 
 const count = 1
-export function fizzBuzz(count) {
+
+function fizzBuzz(count) {
     if(count % 3 === 0 && count % 5 === 0){
         return ('fizzbuzz');
     } else if (count % 5 === 0){
